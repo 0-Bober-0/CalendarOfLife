50e36bcb571b67969541abe12b516a4281031c14
Bober <s-daew@ya.ru>
2025-06-15 01:30:30 +0300

    adding a telegram bot

--- TelegramBot/src/test/java/com/calendar/telegrambot/TelegramBotApplicationTests.java
+++ TelegramBot/src/test/java/com/calendar/telegrambot/TelegramBotApplicationTests.java
@@ -1,10 +1,10 @@
-package com.calendar.server;
+package com.calendar.telegrambot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class TelegramBotApplicationTests {
 
 	@Test
 	void contextLoads() {
