2b3e3e3fe3a8095337321fb505ebbb8b93d3953f
Leonid <leonidplatonov23@gmail.com>
2025-06-15 00:18:49 +0300

    update

--- Server/src/main/java/com/calendar/server/Kafka/UserNotification.java
+++ Server/src/main/java/com/calendar/server/Kafka/UserNotification.java
@@ -1,4 +1,16 @@
 package com.calendar.server.Kafka;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserNotification {
+    private Long chatId;
+    private String message;
+    private LocalDateTime timestamp;
 }
