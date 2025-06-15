4dbd714ccef5edd16d1164d4c54080d7120a9547
Leonid <leonidplatonov23@gmail.com>
2025-06-15 13:46:01 +0300

    update

---
+++ Server/src/main/java/com/calendar/server/kafka/WeekCalculationConsumer.java
@@ -0,0 +1,34 @@
+package com.calendar.server.kafka;
+
+import com.calendar.server.dto.WeekCalculationResult;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class UserDataConsumer {
+    private final KafkaTemplate<String, WeekCalculationResult> resultTemplate;
+
+    @KafkaListener(topics = "${kafka.topic.user-data}", groupId = "week-calculation-group")
+    public void calculateWeeks(User user) {
+        long weeks = ChronoUnit.WEEKS.between(
+            user.getBirth_date(),
+            LocalDate.now()
+        );
+
+        WeekCalculationResult result = new WeekCalculationResult(
+            user.getChat_id(),
+            weeks,
+            user.getCalculationRequestTime(),
+            LocalDateTime.now()
+        );
+        resultTemplate.send("week-calculation-results", result);
+    }
+}
